5ac6d531fce402565486c468f40dfa7aa4740221
Vishnu Thankappan <vishnunadaar111@gmail.com>
2021-07-31 19:28:02 +0530

    Delete projects directory
    deleting library

--- projects/ngx-mat-hover-scroll/src/lib/ngx-mat-hover-scroll.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef } from '@angular/core';
-import { NgxMatHoverScrollDirective } from './ngx-mat-hover-scroll.directive';
-
-describe('NgxMatHoverScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgxMatHoverScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/ngx-mat-hover-scroll/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-mat-hover-scroll
- */
-
-export * from './lib/ngx-mat-hover-scroll.service';
-export * from './lib/ngx-mat-hover-scroll.directive';
-export * from './lib/ngx-mat-hover-scroll.module';
